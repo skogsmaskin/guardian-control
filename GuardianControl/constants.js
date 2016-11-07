11d68518971aef97710f776b85b26ffaded9e146
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2016-11-07 14:12:16 +0100

    Constants

---
+++ GuardianControl/constants.js
@@ -0,0 +1,7 @@
+export const APP_NAME = 'GuardianControl'
+export const BLE_CHARACTERISTIC_UUID = '0000fff1-0000-1000-8000-00805f9b34fb'
+export const BLE_SERVICE_UUID = '0000fff0-0000-1000-8000-00805f9b34fb'
+export const BLE_SERVICE_HEX = 'fff0'
+export const BLE_SCAN_TIME = 10 // Seconds
+export const BLE_DISCOVERED_DEVICE_TTL = 10 // Seconds
+export const CONNECTED_DEVICE_STORAGE_KEY = `@${APP_NAME}:device`
